b9f8c04e1e3f0906144e14b732624b1c3e545d38
KamariRoss <kamarimobile@gmail.com>
2020-05-03 15:23:17 -0400

    Adding Commits

---
+++ Card.cs
@@ -0,0 +1,14 @@
+namespace DeckOfCards
+{
+  public class Card
+  {
+    // vale
+    public int value { get; set; }
+    // rank
+    public string Rank { get; set; }
+    // suit
+    public string suit { get; set; }
+    // color
+    public string ColorOfTheCard { get; set; }
+  }
+}
